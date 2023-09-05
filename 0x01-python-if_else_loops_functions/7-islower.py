12a08007345abddd4a4cb3aee0223d9bc2bc9053
Green <greenderrickngugi@gmail.com>
2023-09-05 14:37:04 -0700

    7-islower.py

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def islower(c):
+    for i in range(97, 123):
+        if chr(i) == c:
+            result = True
+            return result
+        else:
+            result = False
+            return result
