f22f84ec36d8868369518b67c60552aedf4775d0
Green <greenderrickngugi@gmail.com>
2023-09-06 17:48:16 -0700

    do not print numbers that wehn reversed are of same value

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for i in range(00, 90):
-    if i < 89:
-        print("{:02d}".format(i), end=(', '))
-    else:
-        print("{}".format(i))
+for i in range(0, 9):
+    for j in range (0, 9):
+        if (i, j) != (j, i):
+            print("{:01d}{:01d}".format(i, j), end=(', '))
