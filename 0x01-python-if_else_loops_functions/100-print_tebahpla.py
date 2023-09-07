2d5e983f23f65ddb53cb269282080cf26fb45ac2
Green <greenderrickngugi@gmail.com>
2023-09-06 17:30:38 -0700

    Alternate uppercase and lowercase

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 a = 98
 b = 122
-
 while b > 96:
     if b % 2 != 0:
         res = chr(b - 32)
     else:
         res = chr(b)
-    print(f"{res}", end=(''))
+    print("{}".format(res), end=(''))
     b -= 1
