40a89745463165f84b465af7699bb47c57dcf889
Green <greenderrickngugi@gmail.com>
2023-09-07 17:45:47 -0700

    calculator module

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,4 +6,4 @@ if __name__ == '__main__':
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} + {} = {}".format(a, b, sub(a, b)))
     print("{} + {} = {}".format(a, b, mult(a, b)))
-    print("{} + {} = {:.0f}".format(a, b, div(a, b)))
+    print("{} + {} = {}".format(a, b, div(a, b)))
