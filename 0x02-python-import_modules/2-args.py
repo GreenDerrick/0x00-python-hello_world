d3a041cb71c564f8631be63331cf8aa840cb8fb5
Green <greenderrickngugi@gmail.com>
2023-09-07 18:46:42 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,11 +8,8 @@ if __name__ == '__main__':
 
     if argc == 0:
         print("{} arguments.".format(argc))
-    elif argc == 1:
-        print("{}: argument.".format(argc))
-        print("{}: {}".format(i, argv[i]))
-    else:
-        print("{} arguments".format(argc))
+    elif argc > 0:
+        print("{} arguments:".format(argc))
         while i <= argc:
             print("{}: {}".format(i, argv[i]))
             i += 1
